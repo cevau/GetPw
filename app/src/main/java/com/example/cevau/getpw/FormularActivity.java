2858842a8dd32443850a54cffa6d4e7471bafece
christoph voigt <christoph voigt>
2016-08-02 15:48:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cevau/getpw/FormularActivity.java
@@ -0,0 +1,15 @@
+package com.example.cevau.getpw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+//todo make ready for language change
+public class FormularActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.formular_activity);
+    }
+}
